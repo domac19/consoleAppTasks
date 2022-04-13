8f4137ade8f392acd7e221b533a5d1784e2af8c0
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2022-04-13 11:56:53 +0200

    Add files via upload

---
+++ calculateBigNumbers/calculateBigNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace calculateBigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dodaj dva velika broja i pogledaj zbroj!");
+            ulong firstBigNumber = (UInt64)Convert.ToInt64(Console.ReadLine());
+            ulong secondBigNumber = (UInt64)Convert.ToInt64(Console.ReadLine());
+
+            var calculate = firstBigNumber + secondBigNumber;
+
+            Console.WriteLine((UInt64)calculate);
+        }
+    }
+}
