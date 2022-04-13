c40141d4678fe32c346db39b36c9b443f3f563a0
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2022-04-13 12:01:09 +0200

    Add files via upload

---
+++ ReverseVariables/ReverseVariables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            Console.WriteLine("Ako zamjenimo mjesta brojevima " + a + " i " + b);
+
+            a = a + b; 
+            b = a - b; 
+            a = a - b; 
+            Console.WriteLine("Dobijemo: " + a + " i " + b);
+        }
+    }
+}
