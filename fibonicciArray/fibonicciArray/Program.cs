93eb1049962c8cc7fbbbd888abe9669595b20f05
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2022-04-13 11:58:47 +0200

    Add files via upload

---
+++ fibonicciArray/fibonicciArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace fibonicciArray
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonicciArray = new Fibonincci();
+            fibonicciArray.GetFibonicciArray();
+        }
+    }
+}
