758aafed0917540f98c1919edf24eb4d0e7b59d8
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2022-04-13 11:54:28 +0200

    Add files via upload

---
+++ divideBy/divideBy/DivideBy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace divideBy
+{
+    partial class Program
+    {
+        public class DivideBy
+        {
+            public int GetHighNumberOfDividion()
+            {
+                Console.WriteLine("Enter one number to see first big number divided by 3");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                do
+                {
+                    number++;
+                } while (number % 3 != 0);
+
+                return number;
+            }
+        }
+    }
+}
---
+++ divideBy/divideBy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace divideBy
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var divideBy = new DivideBy();
+            Console.WriteLine(divideBy.GetHighNumberOfDividion());
+        }
+    }
+}
