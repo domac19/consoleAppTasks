525dc211af9685a1887a0dcab8802311d5c07651
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2022-04-13 11:52:56 +0200

    Add files via upload

---
+++ factoriels/factoriels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace factoriels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write one number to see factorial of it.");
+
+            int number = 1;
+            var inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                number = number * i;
+            }
+            Console.WriteLine("Factoriel of your number is " + number);
+        }
+    }
+}
